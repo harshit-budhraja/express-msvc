fa9e4734cb4ad2ead1d4ee5aeb53523488af6352
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2021-02-25 23:14:16 +0530

    Generic Error Handler @todo

---
+++ src/utils/generic_error_handler.js
@@ -0,0 +1,10 @@
+const { utilities } = global;
+const { logger } = utilities;
+
+/**
+ * @todo
+ * 
+ * Write a generic error handler which could
+ * be plugged into any catch-block and it drives
+ * loggings, throw behaviour for them etc.
+ */
